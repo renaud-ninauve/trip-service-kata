7b04e4e9a34416dbb5b0e62e4980e81cda89fb1d
renaud-ninauve <renaud.ninauve.92@gmail.com>
2024-09-27 10:52:46 +0200

    throw_when_not_logged

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,36 @@
 package org.craftedsw.tripservicekata.trip;
 
-public class TripServiceTest {
-	
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.LoggedUserHolder;
+import org.craftedsw.tripservicekata.user.User;
+import org.craftedsw.tripservicekata.user.UserTrips;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ExtendWith(MockitoExtension.class)
+class TripServiceTest {
+    TripService tripService;
+    @Mock
+    LoggedUserHolder loggedUserHolder;
+    @Mock
+    UserTrips userTrips;
+
+    @BeforeEach
+    void setup() {
+        tripService = new TripService();
+        tripService.setLoggedUserHolder(loggedUserHolder);
+        tripService.setUserTrips(userTrips);
+    }
+
+    @Test
+    void throw_when_not_logged() {
+        assertThrows(UserNotLoggedInException.class, () -> {
+            tripService.getTripsByUser(new User());
+        });
+    }
 }
