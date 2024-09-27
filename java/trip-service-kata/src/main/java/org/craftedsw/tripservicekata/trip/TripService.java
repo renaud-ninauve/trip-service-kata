4c5f03e09b1b6d22d075d173954e42931feb5a18
renaud-ninauve <renaud.ninauve.92@gmail.com>
2024-09-27 10:19:21 +0200

    extract variable loggedUserHolder

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -11,7 +11,8 @@ public class TripService {
 
 	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
 		List<Trip> tripList = new ArrayList<Trip>();
-		User loggedUser = new LoggedUserHolder().getLoggedUser();
+		LoggedUserHolder loggedUserHolder = new LoggedUserHolder();
+		User loggedUser = loggedUserHolder.getLoggedUser();
 		boolean isFriend = false;
 		if (loggedUser != null) {
 			for (User friend : user.getFriends()) {
