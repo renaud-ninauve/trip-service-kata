f7dbbbc84c4bd27e38655740e2501edec62e894a
renaud-ninauve <renaud.ninauve.92@gmail.com>
2024-09-27 10:17:11 +0200

    move getLoggedUser() to LoggedUserHolder

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -4,14 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.LoggedUserHolder;
 import org.craftedsw.tripservicekata.user.User;
-import org.craftedsw.tripservicekata.user.UserSession;
 
 public class TripService {
 
 	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
 		List<Trip> tripList = new ArrayList<Trip>();
-		User loggedUser = getLoggedUser();
+		User loggedUser = LoggedUserHolder.getLoggedUser();
 		boolean isFriend = false;
 		if (loggedUser != null) {
 			for (User friend : user.getFriends()) {
@@ -27,10 +27,6 @@ public class TripService {
 		} else {
 			throw new UserNotLoggedInException();
 		}
-	}
-
-	private static User getLoggedUser() {
-		return UserSession.getInstance().getLoggedUser();
 	}
 
 }
--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/user/LoggedUserHolder.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/user/LoggedUserHolder.java
@@ -1,4 +1,7 @@
 package org.craftedsw.tripservicekata.user;
 
 public class LoggedUserHolder {
+    public static User getLoggedUser() {
+        return UserSession.getInstance().getLoggedUser();
+    }
 }
